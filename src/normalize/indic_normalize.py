0ae00df23be4c2918d54f167e0c839066437fdd7
Anoop Kunchukuttan <anoop.kunchukuttan@gmail.com>
2013-09-17 18:17:50 +0530

    GPL License

--- src/normalize/indic_normalize.py
+++ src/normalize/indic_normalize.py
@@ -1,4 +1,22 @@
-# Program for normalization of text written in Unicode. This is mainly geared towards Indic scripts 
+# Copyright Anoop Kunchukuttan 2013 - present
+#
+# This file is part of Indic NLP Library.
+# 
+# Indic NLP LIbrary is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Foobar is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#        GNU General Public License for more details.
+# 
+#        You should have received a copy of the GNU General Public License
+#        along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#Program for normalization of text written in Unicode. This is mainly geared towards Indic scripts 
 #
 # @author Anoop Kunchukuttan 
 #
