88f705f397e7ec0886fe4b9fc5320437c71b762a
Anoop <anoop.kunchukuttan@gmail.com>
2014-08-17 23:50:24 +0530

    License info updated

--- src/indicnlp/normalize/indic_normalize.py
+++ src/indicnlp/normalize/indic_normalize.py
@@ -2,7 +2,7 @@
 #
 # This file is part of Indic NLP Library.
 # 
-# Indic NLP LIbrary is free software: you can redistribute it and/or modify
+# Indic NLP Library is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
--- src/indicnlp/transliterate/unicode_transliterate.py
+++ src/indicnlp/transliterate/unicode_transliterate.py
@@ -4,18 +4,18 @@ import string
 #
 # This file is part of Indic NLP Library.
 # 
-# Indic NLP LIbrary is free software: you can redistribute it and/or modify
+# Indic NLP Library is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 # 
-# Foobar is distributed in the hope that it will be useful,
+# Indic NLP Library is distributed in the hope that it will be useful,
 #        but WITHOUT ANY WARRANTY; without even the implied warranty of
 #        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #        GNU General Public License for more details.
 # 
 #        You should have received a copy of the GNU General Public License
-#        along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#        along with Indic NLP Library.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 #Program for text written in one Indic script to another based on Unicode mappings. 
