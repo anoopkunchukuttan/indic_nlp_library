db1343bdb278405bdddb825e99dc6cb91329ab8a
Anoop Kunchukuttan <anoop.kunchukuttan@gmail.com>
2014-10-14 18:17:34 +0530

    Bug fix: committed untracked file

---
+++ src/indicnlp/langinfo.py
@@ -0,0 +1,40 @@
+# Copyright Anoop Kunchukuttan 2014 - present
+#
+# This file is part of Indic NLP Library.
+# 
+# Indic NLP Library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Indic NLP Library is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#        GNU General Public License for more details.
+# 
+#        You should have received a copy of the GNU General Public License
+#        along with Indic NLP Library.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+SCRIPT_RANGES={
+                 'pa':[0x0a00,0x0a7f] ,  
+                 'gu':[0x0a80,0x0aff] ,  
+                 'or':[0x0b00,0x0b7f] ,  
+                 'ta':[0x0b80,0x0bff] ,  
+                 'te':[0x0c00,0x0c7f] ,  
+                 'kn':[0x0c80,0x0cff] ,  
+                 'ml':[0x0d00,0x0d7f] ,  
+                 'hi':[0x0900,0x097f] ,  
+                 'mr':[0x0900,0x097f] ,   
+                 'kK':[0x0900,0x097f] ,   
+                 'sa':[0x0900,0x097f] ,   
+                 'ne':[0x0900,0x097f] ,   
+                 'bn':[0x0980,0x09ff] ,  
+                 'as':[0x0980,0x09ff] ,  
+              }
+
+COORDINATED_RANGE_START_INCLUSIVE=0
+COORDINATED_RANGE_END_INCLUSIVE=0x6f
+
+NUMERIC_OFFSET_START=0x66
+NUMERIC_OFFSET_END=0x6f
