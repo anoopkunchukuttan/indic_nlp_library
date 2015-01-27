551475f1958c3fe66908b12fc5648fe4874f406c
Anoop Kunchukuttan <anoop.kunchukuttan@gmail.com>
2015-01-27 12:34:05 +0530

    (1) bug fixes realted to marker paramter. (2) morph_analyze function now checks if morphanalysis is needed

--- src/indicnlp/morph/unsupervised_morph.py
+++ src/indicnlp/morph/unsupervised_morph.py
@@ -69,12 +69,26 @@ class UnsupervisedMorphAnalyzer(MorphAnalyzerI):
 
         @param word: string input word 
         """
-        val=self._morfessor_model.viterbi_segment(word)
-        m_list=val[0]
-        if add_marker:
-            m_list= [ u'{}_S_'.format(m) if i>0 else u'{}_R_'.format(m)  for i,m in enumerate(m_list)]
-        return m_list
+        m_list=[]
+        if self._morphanalysis_needed(word): 
+            val=self._morfessor_model.viterbi_segment(word)
+            m_list=val[0]
+            if self.add_marker:
+                m_list= [ u'{}_S_'.format(m) if i>0 else u'{}_R_'.format(m)  for i,m in enumerate(m_list)]
+        else:
+            if self.add_marker:
+                word=u'{}_E_'.format(word)
+            mlist=[word]
+        return m_list 
+
+        ### Older implementation
+        #val=self._morfessor_model.viterbi_segment(word)
+        #m_list=val[0]
+        #if self.add_marker:
+        #    m_list= [ u'{}_S_'.format(m) if i>0 else u'{}_R_'.format(m)  for i,m in enumerate(m_list)]
+        #return m_list
     
+
     def morph_analyze_document(self,tokens):
         """
         Morphanalyzes a document, represented as a list of tokens
@@ -84,16 +98,24 @@ class UnsupervisedMorphAnalyzer(MorphAnalyzerI):
 
         @return seuqence of morphemes 
         """
+
         out_tokens=[]
         for token in tokens: 
-            if self._morphanalysis_needed(token): 
-                morphs=self.morph_analyze(token)
-                out_tokens.extend(morphs)
-            else:
-                if add_marker:
-                    token=u'{}_E_'.format(token)
-                out_tokens.append(token)
+            morphs=self.morph_analyze(token)
+            out_tokens.extend(morphs)
         return out_tokens    
+
+        #### Older implementation
+        #out_tokens=[]
+        #for token in tokens: 
+        #    if self._morphanalysis_needed(token): 
+        #        morphs=self.morph_analyze(token)
+        #        out_tokens.extend(morphs)
+        #    else:
+        #        if self.add_marker:
+        #            token=u'{}_E_'.format(token)
+        #        out_tokens.append(token)
+        #return out_tokens    
 
 
 if __name__ == '__main__': 
@@ -108,7 +130,7 @@ if __name__ == '__main__':
     add_marker=False
 
     if len(sys.argv)==6:
-        add_marker=bool(sys.argv[5])
+        add_marker= True if sys.argv[5] == 'True' else False 
 
     analyzer=UnsupervisedMorphAnalyzer(language,add_marker)
 
