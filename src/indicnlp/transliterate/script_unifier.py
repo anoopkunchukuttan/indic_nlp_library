b70df6404f215066c85760161357ee5ba5fe38a4
Anoop Kunchukuttan <anoop.kunchukuttan@gmail.com>
2019-11-28 16:40:35 +0530

    new script unifier

---
+++ src/indicnlp/transliterate/script_unifier.py
@@ -0,0 +1,13 @@
+class AggresiveScriptUnifier():
+    def __init__(self,common_lang):
+        self.normalizer_map={}
+        self._init_normalizers()
+
+    def _init_normalizers(self):
+        pass 
+
+    def _init_transliterators(self):    
+        pass 
+
+    def transform(text,lang):
+	pass 
