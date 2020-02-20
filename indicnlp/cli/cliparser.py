4b32c007f43c062703628abf30c038b0234ddcbd
Anoop Kunchukuttan <ankunchu@microsoft.com>
2020-02-20 01:27:48 -0800

    removed unecessary comments

--- indicnlp/cli/cliparser.py
+++ indicnlp/cli/cliparser.py
@@ -227,37 +227,3 @@ if __name__ == '__main__':
     loader.load()
     main()
 
-    # # fmt: off
-    # parser.add_argument('--no-progress-bar', action='store_true', help='disable progress bar')
-    # parser.add_argument('--log-interval', type=int, default=1000, metavar='N',
-    #                     help='log progress every N batches (when progress bar is disabled)')
-    # parser.add_argument('--log-format', default=None, help='log format to use',
-    #                     choices=['json', 'none', 'simple', 'tqdm'])
-    # parser.add_argument('--tensorboard-logdir', metavar='DIR', default='',
-    #                     help='path to save logs for tensorboard, should match --logdir '
-    #                          'of running tensorboard (default: no tensorboard logging)')
-    # parser.add_argument('--seed', default=1, type=int, metavar='N',
-    #                     help='pseudo random number generator seed')
-    # parser.add_argument('--cpu', action='store_true', help='use CPU instead of CUDA')
-    # parser.add_argument('--fp16', action='store_true', help='use FP16')
-    # parser.add_argument('--memory-efficient-fp16', action='store_true',
-    #                     help='use a memory-efficient version of FP16 training; implies --fp16')
-    # parser.add_argument('--fp16-no-flatten-grads', action='store_true',
-    #                     help='don\'t flatten FP16 grads tensor')
-    # parser.add_argument('--fp16-init-scale', default=2 ** 7, type=int,
-    #                     help='default FP16 loss scale')
-    # parser.add_argument('--fp16-scale-window', type=int,
-    #                     help='number of updates before increasing loss scale')
-    # parser.add_argument('--fp16-scale-tolerance', default=0.0, type=float,
-    #                     help='pct of updates that can overflow before decreasing the loss scale')
-    # parser.add_argument('--min-loss-scale', default=1e-4, type=float, metavar='D',
-    #                     help='minimum FP16 loss scale, after which training is stopped')
-    # parser.add_argument('--threshold-loss-scale', type=float,
-    #                     help='threshold FP16 loss scale from below')
-    # parser.add_argument('--user-dir', default=None,
-    #                     help='path to a python module containing custom extensions (tasks and/or architectures)')
-    # parser.add_argument('--empty-cache-freq', default=0, type=int,
-    #                     help='how often to clear the PyTorch CUDA cache (0 to disable)')
-    # parser.add_argument('--all-gather-list-size', default=16384, type=int,
-    #                     help='number of bytes reserved for gathering stats from workers')
-
